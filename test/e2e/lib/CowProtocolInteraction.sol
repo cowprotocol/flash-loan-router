d5b897a6e0fe512f3f43965f12fbf50acf4d7d5b
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2025-03-05 14:43:03 +0000

    Add ERC-3156 borrower

---
+++ test/e2e/lib/CowProtocolInteraction.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8;
+
+import {Vm} from "forge-std/Test.sol";
+
+import {IBorrower, ICowSettlement, IERC20} from "src/interface/IBorrower.sol";
+
+import {TokenBalanceAccumulator} from "./TokenBalanceAccumulator.sol";
+
+library CowProtocolInteraction {
+    function transferFrom(IERC20 token, address from, address to, uint256 amount)
+        internal
+        pure
+        returns (ICowSettlement.Interaction memory)
+    {
+        return ICowSettlement.Interaction({
+            target: address(token),
+            value: 0,
+            callData: abi.encodeCall(IERC20.transferFrom, (from, to, amount))
+        });
+    }
+
+    function transfer(IERC20 token, address to, uint256 amount)
+        internal
+        pure
+        returns (ICowSettlement.Interaction memory)
+    {
+        return ICowSettlement.Interaction({
+            target: address(token),
+            value: 0,
+            callData: abi.encodeCall(IERC20.transfer, (to, amount))
+        });
+    }
+
+    function wrapperApprove(IBorrower solverWrapper, IERC20 token, address spender, uint256 amount)
+        internal
+        pure
+        returns (ICowSettlement.Interaction memory)
+    {
+        return ICowSettlement.Interaction({
+            target: address(solverWrapper),
+            value: 0,
+            callData: abi.encodeCall(IBorrower.approve, (token, spender, amount))
+        });
+    }
+
+    function pushBalanceToAccumulator(TokenBalanceAccumulator tokenBalanceAccumulator, IERC20 token, address owner)
+        internal
+        pure
+        returns (ICowSettlement.Interaction memory)
+    {
+        return ICowSettlement.Interaction({
+            target: address(tokenBalanceAccumulator),
+            value: 0,
+            callData: abi.encodeCall(TokenBalanceAccumulator.push, (token, owner))
+        });
+    }
+}
