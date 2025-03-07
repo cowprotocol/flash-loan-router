5c98fbf99ab529c8e27655c279be98610da32ba4
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-03-07 09:35:21 +0000

    Update deployment scripts

--- script/DeployAAVEBorrower.s.sol
+++ script/DeployAAVEBorrower.s.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.28;
 
+import {AaveBorrower} from "../src/AaveBorrower.sol";
+import {FlashLoanRouter} from "../src/FlashLoanRouter.sol";
+import {ICowSettlement} from "../src/interface/ICowSettlement.sol";
 import "forge-std/Script.sol";
-import {Utils} from "script/libraries/Utils.sol";
 import {EnvReader} from "script/libraries/EnvReader.sol";
-import { ICowSettlement } from "../src/interface/ICowSettlement.sol";
-import { IBorrower } from "../src/interface/IBorrower.sol";
-import { FlashLoanRouter } from "../src/FlashLoanRouter.sol";
-import { AaveBorrower } from "../src/AaveBorrower.sol";
+import {Utils} from "script/libraries/Utils.sol";
 
 contract DeployAAVEBorrower is Script, EnvReader, Utils {
     constructor() {
         // Initialise contracts
-        settlement = addressEnvOrDefault("SETTLEMENT_CONTRACT", DEFAULT_SETTLEMENT_CONTRACT);
-        console.log("Settlement contract at %s.", settlement);
-        require(settlement != address(0), "Invalid settlement contract address.");
-        assertHasCode(settlement, "no code at expected settlement contract");
+        settlement = addressEnvOrDefault("SETTLEMENT_CONTRACT", DEFAULT_SETTLEMENT_CONTRACT, false);
     }
 
     function run() public {
@@ -23,20 +19,16 @@ contract DeployAAVEBorrower is Script, EnvReader, Utils {
         require(deployerPrivateKey != 0, "Missing or invalid PRIVATE_KEY.");
 
         vm.startBroadcast(deployerPrivateKey);
-        
+
         // Deploy router
         ICowSettlement cowSettlement = ICowSettlement(settlement);
-        FlashLoanRouter router = new FlashLoanRouter(cowSettlement);
+        FlashLoanRouter router = new FlashLoanRouter{salt: SALT}(cowSettlement);
         console.log("FlashLoanRouter deployed at:", address(router));
-        
-        // Deploy borrower
-        AaveBorrower aaveBorrower = new AaveBorrower(router);
-        console.log("AaveBorrower deployed at:", address(aaveBorrower));
-        
-        IBorrower borrower = IBorrower(aaveBorrower);
-        require(borrower.settlementContract() == cowSettlement, "Settlement contract incorrectly set");
-        require(borrower.router() == router, "Router contract incorrectly set");
 
+        // Deploy borrower
+        AaveBorrower borrower = new AaveBorrower{salt: SALT}(router);
+        console.log("AaveBorrower deployed at:", address(borrower));
+        
         vm.stopBroadcast();
     }
 }
