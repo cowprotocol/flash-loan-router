d1c91bf47ccdbacbc34a6db2444734033f7afd76
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-03-07 11:28:01 +0000

    Split deployment scripts into single deployment and script for all

--- script/DeployAllContracts.s.sol
+++ script/DeployAllContracts.s.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.28;
 
-import {Script, console} from "forge-std/Script.sol";
-import {DeployFlashLoanRouter, FlashLoanRouter} from "./single-deployment/DeployFlashLoanRouter.s.sol";
-import {DeployAAVEBorrower, AaveBorrower} from "./single-deployment/DeployAAVEBorrower.s.sol";
 import {EnvReader} from "./libraries/EnvReader.sol";
+import {AaveBorrower, DeployAAVEBorrower} from "./single-deployment/DeployAAVEBorrower.s.sol";
+import {DeployFlashLoanRouter, FlashLoanRouter} from "./single-deployment/DeployFlashLoanRouter.s.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract DeployAllContracts is DeployFlashLoanRouter, DeployAAVEBorrower {
     function run() public override(DeployFlashLoanRouter, DeployAAVEBorrower) {
         deployAll();
     }
 
-    function deployAll() public returns(FlashLoanRouter flashLoanRouter, AaveBorrower aaveBorrower) {
+    function deployAll() public returns (FlashLoanRouter flashLoanRouter, AaveBorrower aaveBorrower) {
         flashLoanRouter = deployFlashLoanRouter();
         aaveBorrower = deployAAVEBorrower();
 
--- script/single-deployment/DeployFlashLoanRouter.s.sol
+++ script/single-deployment/DeployFlashLoanRouter.s.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.28;
 
-import {Script, console} from "forge-std/Script.sol";
 import {FlashLoanRouter} from "../../src/FlashLoanRouter.sol";
 import {ICowSettlement} from "../../src/interface/ICowSettlement.sol";
 import {EnvReader} from "../libraries/EnvReader.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract DeployFlashLoanRouter is Script, EnvReader {
     function run() public virtual {
