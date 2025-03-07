2ded7656574a005e580658bf7774a93b9083961f
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-03-07 15:44:02 +0000

    Update imports

--- script/DeployAllContracts.s.sol
+++ script/DeployAllContracts.s.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.28;
 
-import {EnvReader} from "./libraries/EnvReader.sol";
+import {Script, console} from "forge-std/Script.sol";
+
 import {AaveBorrower, DeployAAVEBorrower} from "./single-deployment/DeployAAVEBorrower.s.sol";
 import {DeployFlashLoanRouter, FlashLoanRouter} from "./single-deployment/DeployFlashLoanRouter.s.sol";
-import {Script, console} from "forge-std/Script.sol";
+
+import {EnvReader} from "./libraries/EnvReader.sol";
 
 contract DeployAllContracts is DeployFlashLoanRouter, DeployAAVEBorrower {
     function run() public override(DeployFlashLoanRouter, DeployAAVEBorrower) {
