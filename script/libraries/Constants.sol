bf8e29b18d6c93590551a8009e4ed949e02fb923
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-03-19 09:50:57 +0000

    Update ignore run-latest.json broadcast files

--- script/libraries/Constants.sol
+++ script/libraries/Constants.sol
@@ -6,5 +6,5 @@ pragma solidity ^0.8;
 /// @notice A library defining constant variables used for smart contract deployments.
 library Constants {
     address internal constant DEFAULT_SETTLEMENT_CONTRACT = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41;
-    bytes32 internal constant SALT = bytes32(uint256(1001));
+    bytes32 internal constant SALT = bytes32(uint256(1003));
 }
