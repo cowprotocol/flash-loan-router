319db582dd3cefacd6afd24960f4b1589894b2f7
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-03-19 09:55:13 +0000

    Add script to generate contract addresses networks.json file

--- dev/generate-networks-file.sh
+++ dev/generate-networks-file.sh
@@ -5,23 +5,21 @@ set -o errexit -o pipefail -o nounset
 repo_root_dir="$(git rev-parse --show-toplevel)"
 
 for deployment in "$repo_root_dir/broadcast/"*"/"*"/"*".json"; do
-  # Extract the chain ID from the folder structure
+  # The subfolder name is the chain id
   chain_id=${deployment%/*}
   chain_id=${chain_id##*/}
 
-  # Process each deployment file and format it correctly
+  # First, every single deployment is formatted as if it had its own networks.json
   jq --arg chainId "$chain_id" '
     .transactions[]
-    | select(.transactionType == "CREATE")
+    | select(.transactionType == "CREATE2")
     | select(.hash != null)
     | {(.contractName): {($chainId): {address: .contractAddress, transactionHash: .hash }}}
-  ' <"$deployment"
+  '  <"$deployment"
 done \
-  | # Merge all contracts ensuring multiple chains are properly stored
-    jq --sort-keys --null-input '
-      reduce inputs as $item ({}; 
-        . as $orig | 
-        reduce ($item | to_entries[]) as $kv ($orig; 
-          .[$kv.key] += $kv.value
-        )
-      )'
+  | # Then, all these single-contract single-chain-id networks.jsons are merged. Note: in case the same contract is
+    # deployed twice in the same script run, the last deployed contract takes priority.
+    # If the same contract is deployed twice in different runs, the address in the file path that comes latest in
+    # alphabetical order takes priority. For example, a contract in `broadcast/Deployment10/*` is overwritten by
+    # one with the same name from `broadcast/Deployment2/*`.
+    jq --sort-keys --null-input 'reduce inputs as $item ({}; . *= $item)'
