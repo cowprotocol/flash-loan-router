64f4f57553a6bd5c77919dc8694d5c68b183837a
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-03-20 08:19:28 +0000

    Update natspecs

--- script/single-deployment/DeployFlashLoanRouter.s.sol
+++ script/single-deployment/DeployFlashLoanRouter.s.sol
@@ -16,35 +16,32 @@ contract DeployFlashLoanRouter is Script {
         deployFlashLoanRouter();
     }
 
-    /**
-    * @dev Deploys the FlashLoanRouter contract.
-    * 
-    * This function deploys the `FlashLoanRouter` contract with a deterministic address 
-    * using the `CREATE2` opcode.
-    * The contract is initialized with a `cowSettlement` contract, which is fetched 
-    * from the `Constants.DEFAULT_SETTLEMENT_CONTRACT`.
-    * 
-    * The deployment is done using a fixed salt (`Constants.SALT`), ensuring that 
-    * the address of the `FlashLoanRouter` is deterministic.
-    * The function will log the address of the newly deployed `FlashLoanRouter` 
-    * contract for reference.
-    * 
-    * @return router The deployed `FlashLoanRouter` contract instance.
-    * 
-    * @notice The FlashLoanRouter contract address is generated using `CREATE2` 
-    *         with a deterministic salt (e.g., `new FlashLoanRouter{salt: Constants.SALT}(cowSettlement)`), 
-    *         and the simulation of `AaveBorrower` deployment will revert with a `CREATE2` 
-    *         collision error when deploying all contracts, if there is already 
-    *         an existing contract at the same address.
-    *         This issue is avoided by passing the address directly as an environment 
-    *         variable (`FLASHLOAN_ROUTER_ADDRESS`).
-    */
+    /// @dev Deploys the FlashLoanRouter contract.
+    ///
+    /// This function deploys the `FlashLoanRouter` contract with a deterministic address
+    /// using the `CREATE2` opcode.
+    /// The contract is initialized with a `cowSettlement` contract, which is fetched
+    /// from the `Constants.DEFAULT_SETTLEMENT_CONTRACT`.
+    ///
+    /// The deployment is done using a fixed salt (`Constants.SALT`), ensuring that
+    /// the address of the `FlashLoanRouter` is deterministic.
+    /// The function will log the address of the newly deployed `FlashLoanRouter`
+    /// contract for reference.
+    ///
+    /// @return router The deployed `FlashLoanRouter` contract instance.
+    ///
+    /// @notice The FlashLoanRouter contract address is generated using `CREATE2`
+    ///         with a deterministic salt (e.g., `new FlashLoanRouter{salt: Constants.SALT}(cowSettlement)`),
+    ///         and the simulation of `AaveBorrower` deployment will revert with a `CREATE2`
+    ///         collision error when deploying all contracts, if there is already
+    ///         an existing contract at the same address.
+    ///         This issue is avoided by passing the address directly as an environment
+    ///         variable (`FLASHLOAN_ROUTER_ADDRESS`).
     function deployFlashLoanRouter() internal returns (FlashLoanRouter router) {
-        // Deploy FlashLoanRouter
         vm.broadcast();
         ICowSettlement cowSettlement = ICowSettlement(Constants.DEFAULT_SETTLEMENT_CONTRACT);
         router = new FlashLoanRouter{salt: Constants.SALT}(cowSettlement);
-        
+
         console.log("FlashLoanRouter deployed at:", address(router));
     }
 }
