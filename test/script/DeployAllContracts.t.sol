097bf1e86a3906fcc99f90448871b012359e35f1
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2025-05-12 07:17:52 +0000

    Separate test for netwoks.json and constant deployment

--- test/script/DeployAllContracts.t.sol
+++ test/script/DeployAllContracts.t.sol
@@ -17,6 +17,12 @@ contract DeployAllContractsTest is Test {
     DeployAllContracts private script;
     CowProtocolMock private cowProtocolMock;
 
+    // Contracts deployed by the deployment script. If you add a new deployment,
+    // remember to update the test `test_consistentNetworksJsonFile`!
+    FlashLoanRouter private router;
+    AaveBorrower private aaveBorrower;
+    ERC3156Borrower private erc3156Borrower;
+
     function setUp() external {
         string memory projectRoot = vm.projectRoot();
         networksJson = vm.readFile(string.concat(projectRoot, "/networks.json"));
@@ -24,22 +30,48 @@ contract DeployAllContractsTest is Test {
         script = new DeployAllContracts();
         cowProtocolMock =
             new CowProtocolMock(vm, address(Constants.SETTLEMENT_CONTRACT), address(Constants.SOLVER_AUTHENTICATOR));
+
+        (router, aaveBorrower, erc3156Borrower) = script.deployAll();
     }
 
-    function test_deterministicDeployments() external {
-        (FlashLoanRouter router, AaveBorrower aaveBorrower, ERC3156Borrower erc3156Borrower) = script.deployAll();
+    function test_unchangedAddresses() external view {
+        // We want to make sure we don't introduce changes that cause the
+        // deterministic addresses of the contract to change.
+        // In particular, the router address is specified in CIP 66. Changing
+        // this address requires a CIP.
+        // https://snapshot.box/#/s:cow.eth/proposal/0x6f3d88347bcc8de87ecded2442c090d8eb1d3ef99eca75a831ee220ff5705f00
+        assertEq(address(router), 0x9da8B48441583a2b93e2eF8213aAD0EC0b392C69);
 
-        // Only test with mainnet. We assume the addresses on the other chains
-        // match the ones on mainnet.
-        uint256 chainId = 1;
-        assertEq(address(router), addressFromNetworksJson("FlashLoanRouter", chainId));
-        assertEq(address(aaveBorrower), addressFromNetworksJson("AaveBorrower", chainId));
-        assertEq(address(erc3156Borrower), addressFromNetworksJson("ERC3156Borrower", chainId));
+        assertEq(address(aaveBorrower), 0x7d9C4DeE56933151Bc5C909cfe09DEf0d315CB4A);
+        assertEq(address(erc3156Borrower), 0x47d71b4B3336AB2729436186C216955F3C27cD04);
     }
 
-    function addressFromNetworksJson(string memory contractName, uint256 chain) private view returns (address) {
-        string memory addressJsonKey = string.concat(".", contractName, ".", vm.toString(chain), ".address");
-        bytes memory data = vm.parseJson(networksJson, addressJsonKey);
-        return abi.decode(data, (address));
+    function test_consistentNetworksJsonFile() external view {
+        assertDeploymentsMatchNetworksJson("FlashLoanRouter", address(router));
+        assertDeploymentsMatchNetworksJson("AaveBorrower", address(aaveBorrower));
+        assertDeploymentsMatchNetworksJson("ERC3156Borrower", address(erc3156Borrower));
+    }
+
+    function assertDeploymentsMatchNetworksJson(string memory contractName, address expected) private view {
+        address[] memory addresses = deploymentAddressesFromNetworksJson(contractName);
+        for (uint256 i = 0; i < addresses.length; i++) {
+            vm.assertEq(addresses[i], expected);
+        }
+    }
+
+    function deploymentAddressesFromNetworksJson(string memory contractName)
+        private
+        view
+        returns (address[] memory addresses)
+    {
+        // The notation ".." matches all subnodes of a node, meaning in this
+        // case each contract deployment for each chain.
+        // https://book.getfoundry.sh/cheatcodes/parse-json?highlight=json#jsonpath-key
+        // https://www.rfc-editor.org/rfc/rfc9535.html#section-2.5.2
+        string memory addressesSelector = string.concat(".", contractName, "..address");
+        bytes memory data = vm.parseJson(networksJson, addressesSelector);
+        addresses = abi.decode(data, (address[]));
+        // Sanity check.
+        vm.assertNotEq(addresses.length, 0);
     }
 }
