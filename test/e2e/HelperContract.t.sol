868ee5ebbaf53f9c1d021682d3e19d4bf8b6b74a
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2025-07-02 17:26:45 +0000

    Use actual order in e2e test

--- test/e2e/HelperContract.t.sol
+++ test/e2e/HelperContract.t.sol
@@ -6,6 +6,8 @@ import {Test, Vm} from "forge-std/Test.sol";
 import {AaveBorrower, IAavePool} from "src/AaveBorrower.sol";
 import {FlashLoanRouter, Loan} from "src/FlashLoanRouter.sol";
 
+import {GPv2Order, IERC20} from "src/helper/GPv2Order.sol";
+import {ISettlement} from "src/helper/ISettlement.sol";
 import {OrderHelper} from "src/helper/OrderHelper.sol";
 import {OrderHelperFactory} from "src/helper/OrderHelperFactory.sol";
 import {ICowSettlement} from "src/interface/IBorrower.sol";
@@ -15,8 +17,14 @@ import {CowProtocol} from "./lib/CowProtocol.sol";
 import {CowProtocolInteraction} from "./lib/CowProtocolInteraction.sol";
 import {ForkedRpc} from "./lib/ForkedRpc.sol";
 
+interface IPreSignTarget {
+    function domainSeparator() external view returns (bytes32);
+    function setPreSignature(bytes calldata orderUid, bool signed) external;
+}
+
 contract E2eHelperContract is Test {
     using ForkedRpc for Vm;
+    using GPv2Order for GPv2Order.Data;
 
     IAavePool internal constant POOL = IAavePool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
     uint256 private constant MAINNET_FORK_BLOCK = 22828430;
@@ -95,6 +103,9 @@ contract E2eHelperContract is Test {
         vm.startPrank(user);
         // Approve the helper to pull the atokens on the swapCollateral hook logic
         Constants.AWETH.approve(address(helper), type(uint256).max);
+        Constants.WETH.approve(
+            address(ISettlement(address(Constants.SETTLEMENT_CONTRACT)).vaultRelayer()), type(uint256).max
+        );
         vm.stopPrank();
 
         // Flashloan definition
@@ -106,38 +117,68 @@ contract E2eHelperContract is Test {
             Sell token: WETH - Buy token: DAI
             from: helper - to: helper
         */
+        GPv2Order.Data memory order = GPv2Order.Data({
+            sellToken: Constants.WETH,
+            buyToken: Constants.DAI,
+            receiver: address(helper),
+            sellAmount: 10 ether,
+            buyAmount: 2_500 ether,
+            validTo: type(uint32).max,
+            appData: bytes32(0),
+            feeAmount: 0,
+            kind: GPv2Order.KIND_SELL,
+            partiallyFillable: false,
+            sellTokenBalance: GPv2Order.BALANCE_ERC20,
+            buyTokenBalance: GPv2Order.BALANCE_ERC20
+        });
+        {
+            bytes memory orderUid = computeOrderUid(order, user);
+            vm.prank(user);
+            IPreSignTarget(address(Constants.SETTLEMENT_CONTRACT)).setPreSignature(orderUid, true);
+        }
 
-        ICowSettlement.Interaction[] memory _interactions = new ICowSettlement.Interaction[](6);
+        ICowSettlement.Interaction[] memory preInteractions = new ICowSettlement.Interaction[](1);
+        ICowSettlement.Interaction[] memory postInteractions = new ICowSettlement.Interaction[](3);
 
-        // 0) Move flashloan from the borrower to the helper
-        _interactions[0] =
-            CowProtocolInteraction.transferFrom(Constants.WETH, address(borrower), address(helper), 10 ether);
+        // 0) Move flashloan from the borrower to the user
+        preInteractions[0] =
+            CowProtocolInteraction.transferFrom(Constants.WETH, address(borrower), address(user), 10 ether);
 
-        // 1) Mock a swap by pulling weth from the helper
-        // hack until order is created propertly we approve helper's weth to the settlement contract
-        vm.prank(address(helper));
-        Constants.WETH.approve(address(Constants.SETTLEMENT_CONTRACT), type(uint256).max);
-        _interactions[1] = CowProtocolInteraction.transferFrom(
-            Constants.WETH, address(helper), address(Constants.SETTLEMENT_CONTRACT), 10 ether
-        );
+        // 1) Mock the "fee payback" by giving flashloan fee to the borrower
+        {
+            uint256 _fee = 5000000000000000; // 10.005 is flashloan + fee. 0.005 is the fee in eth.
+            deal(address(Constants.WETH), address(Constants.SETTLEMENT_CONTRACT), _fee);
+            postInteractions[0] = CowProtocolInteraction.transfer(Constants.WETH, address(borrower), _fee);
+        }
 
-        // 2) Mock the swap by giving DAI to the helper contract
-        deal(address(Constants.DAI), address(Constants.SETTLEMENT_CONTRACT), 2_500 ether);
-        _interactions[2] = CowProtocolInteraction.transfer(Constants.DAI, address(helper), 2_500 ether);
+        // 2) Call helper.swapCollateral()
+        postInteractions[1] = CowProtocolInteraction.orderHelperSwapCollateral(address(helper));
 
-        // 3) Mock the "fee payback" by giving flashloan fee to the borrower
-        uint256 _fee = 5000000000000000; // 10.005 is flashloan + fee. 0.005 is the fee in eth.
-        deal(address(Constants.WETH), address(Constants.SETTLEMENT_CONTRACT), _fee);
-        _interactions[3] = CowProtocolInteraction.transfer(Constants.WETH, address(borrower), _fee);
-
-        // 4) Call helper.swapCollateral()
-        _interactions[4] = CowProtocolInteraction.orderHelperSwapCollateral(address(helper));
-
-        // 5) Borrower needs to approve the pool so the flashloan tokens + fees can be pulled out
-        _interactions[5] =
+        // 3) Borrower needs to approve the pool so the flashloan tokens + fees can be pulled out
+        postInteractions[2] =
             CowProtocolInteraction.borrowerApprove(borrower, Constants.WETH, address(POOL), type(uint256).max);
 
-        bytes memory _settleCallData = CowProtocol.encodeEmptySettleWithInteractions(_interactions);
+        bytes memory _settleCallData;
+        {
+            address[] memory tokens = new address[](2);
+            uint256 wethIndex = 0;
+            uint256 daiIndex = 1;
+            tokens[wethIndex] = address(Constants.WETH);
+            tokens[daiIndex] = address(Constants.DAI);
+
+            uint256[] memory clearingPrices = new uint256[](2);
+            clearingPrices[wethIndex] = order.buyAmount;
+            clearingPrices[daiIndex] = order.sellAmount;
+
+            ICowSettlement.Trade[] memory trades = new ICowSettlement.Trade[](1);
+            trades[0] = derivePreSignTrade(order, wethIndex, daiIndex, user);
+
+            ICowSettlement.Interaction[][3] memory interactions =
+                [preInteractions, new ICowSettlement.Interaction[](0), postInteractions];
+
+            _settleCallData = abi.encodeCall(ICowSettlement.settle, (tokens, clearingPrices, trades, interactions));
+        }
+
         vm.prank(solver);
         router.flashLoanAndSettle(_loans, _settleCallData);
 
@@ -154,5 +195,49 @@ contract E2eHelperContract is Test {
 
         // borrower final state
         assertEq(Constants.WETH.balanceOf(address(borrower)), 0);
+    }
+
+    /// @dev Computes the order UID for an order and the given owner
+    function computeOrderUid(GPv2Order.Data memory order, address owner) internal view returns (bytes memory) {
+        bytes32 domainSeparator = IPreSignTarget(address(Constants.SETTLEMENT_CONTRACT)).domainSeparator();
+        return abi.encodePacked(order.hash(domainSeparator), owner, order.validTo);
+    }
+
+    function derivePreSignTrade(
+        GPv2Order.Data memory order,
+        uint256 sellTokenIndex,
+        uint256 buyTokenIndex,
+        address owner
+    ) internal pure returns (ICowSettlement.Trade memory) {
+        assertEq(order.kind, GPv2Order.KIND_SELL, "Unsupported order kind");
+        assertEq(order.partiallyFillable, false, "Unsupported partially fillable");
+        assertEq(order.sellTokenBalance, GPv2Order.BALANCE_ERC20, "Unsupported order sell token balance");
+        assertEq(order.buyTokenBalance, GPv2Order.BALANCE_ERC20, "Unsupported order buy token balance");
+        bytes memory signature = abi.encodePacked(owner);
+        return ICowSettlement.Trade(
+            sellTokenIndex,
+            buyTokenIndex,
+            order.receiver,
+            order.sellAmount,
+            order.buyAmount,
+            order.validTo,
+            order.appData,
+            order.feeAmount,
+            packFlags(),
+            order.sellAmount,
+            signature
+        );
+    }
+
+    function packFlags() internal pure returns (uint256) {
+        // For information on flag encoding, see:
+        // https://github.com/cowprotocol/contracts/blob/v1.0.0/src/contracts/libraries/GPv2Trade.sol#L70-L93
+        uint256 sellOrderFlag = 0;
+        uint256 fillOrKillFlag = 0 << 1;
+        uint256 internalSellTokenBalanceFlag = 0 << 2;
+        uint256 internalBuyTokenBalanceFlag = 0 << 4;
+        uint256 preSignSignatureFlag = 3 << 5;
+        return sellOrderFlag | fillOrKillFlag | internalSellTokenBalanceFlag | internalBuyTokenBalanceFlag
+            | preSignSignatureFlag;
     }
 }
